5868ab9cf236328871afc62fac58d7e7fa5f936f
Lesley <mutsambiwalesley@gmail.com>
2025-01-21 22:07:04 +0200

    Revamp of  website

--- main.js
+++
@@ -1,6 +0,0 @@
-let hue = 0;
-function changeBackgroundHue() {
-    hue = (hue + 20) % 360;
-    document.body.style.background = `linear-gradient(to bottom, hsl(${hue}, 10%, 50%), hsl(${hue}, 10%, 30%))`;
-}
-setInterval(changeBackgroundHue, 3000);
