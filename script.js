e7c48fef4b3ab6ed4cec102fa0ae7a7f77e2b0d0
Lesley <mutsambiwalesley@gmail.com>
2025-02-01 17:34:13 +0200

    Adding CV

---
+++ script.js
@@ -0,0 +1,23 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+
+menuIcon.onclick = () => {
+    navLinks.classList.toggle('active');
+}
+// menuIcon.addEventListener('click', () => {
+//     navLinks.classList.toggle('active');
+// });
+
+let btnClick = document.querySelector('.visit-btn');
+
+btnClick.addEventListener('click', () => {
+    window.location.href = "https://github.com/Le-e-lab"
+});
+
+const downloadBtn = document.querySelector('#d-btn');
+const fileLink = "id=109LRY3zNTwtNJSIhnPXnK6MJINX-3I52&export=download&authuser=0"
+
+downloadBtn.addEventListener('click', () => {
+    window.location.href = fileLink
+});
